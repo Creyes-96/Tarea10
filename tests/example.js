ef00891386d71dfa396405748779695509a67607
Carlos Reyes <creyes@lomax.com.gt>
2021-08-18 10:56:50 -0600

    Se sube el test Front End finalizado

--- tests/example.js
+++ tests/example.js
@@ -5,7 +5,6 @@ var faker = require('faker');
 function demo(browserType = "chromium") {
     (async () => {
 
-        
 
         //launch a new browser
         const browser = await playwright [browserType].launch({ headless:false});
@@ -23,56 +22,36 @@ function demo(browserType = "chromium") {
         await page.waitForTimeout(2000)
 
         //go to car creation
-        await page.click('text=New Car');
+        for(var i=0; i <=3; i++){
 
-        await page.goto(config.url.base_path + '/cars/new')
-        await page.waitForLoadState();
-        await page.waitForTimeout(2000)
+            await page.click('text=New Car');
+    
+            await page.goto(config.url.base_path + '/cars/new')
+            await page.waitForLoadState();
+            await page.waitForTimeout(2000)
+    
+            //fill inputs
+            page.fill(`#car_model` , faker.datatype.number({min: 1980, max: 2022}).toString())
+            await page.waitForTimeout(200);
+            page.fill(`#car_brand`, faker.vehicle.manufacturer())
+            await page.waitForTimeout(200);
+            page.fill(`#car_driver_id`, faker.datatype.number({min: 0050, max:001000 }).toString())
+            //page.fill(`#car_driver_id`, faker.datatype.hexaDecimal())
+            await page.waitForTimeout(200);
+            page.fill(`#car_code`, faker.datatype.hexaDecimal(5))
+            await page.waitForTimeout(200);
+    
+            page.click('input[name=commit]') //create car
+            await page.waitForTimeout();
+            await page.waitForTimeout(2000);
+    
+            //go to cars index
+            await page.goto(config.url.base_path + '/cars')
 
-        //fill inputs
-        
-        page.fill(`#car_model`, '4042');
-        await page.waitForTimeout(200);
-        //page.fill(`#car_brand`, 'Brand')
-        page.fill(`#car_brand`, faker.vehicle.manufacturer())
-        await page.waitForTimeout(200);
-        page.fill(`#car_driver_id`, '100')
-        //page.fill(`#car_driver_id`, faker.datatype.hexaDecimal())
-        await page.waitForTimeout(200);
-        page.fill(`#car_code`, '1')
-        await page.waitForTimeout(200);
+            i = i++;
+        }
 
-        page.click('input[name=commit]') //create car
-        await page.waitForTimeout();
-        await page.waitForTimeout(2000);
-
-        //go to cars index
-        await page.goto(config.url.base_path + '/cars')
-
-        /*MAS DATOS MAS DATOS MAS DATOS
-        await page.click('text=New Car');
-
-        await page.goto(config.url.base_path + '/cars/new')
-        await page.waitForLoadState();
-        await page.waitForTimeout(2000)
-
-        //fill inputs
-        page.fill(`#car_model`, '4042' );
-        await page.waitForTimeout(200);
-        //page.fill(`#car_brand`, 'Brand')
-        page.fill(`#car_brand`, faker.vehicle.manufacturer())
-        await page.waitForTimeout(200);
-        page.fill(`#car_driver_id`, faker.datatype.hexaDecimal())
-        await page.waitForTimeout(200);
-        page.fill(`#car_code`, '1')
-        await page.waitForTimeout(200);
-
-        page.click('input[name=commit]') //create car
-        await page.waitForTimeout();
-        await page.waitForTimeout(2000);
-
-        //go to cars index
-        await page.goto(config.url.base_path + '/cars')*/
+    
     })()
 }
 
